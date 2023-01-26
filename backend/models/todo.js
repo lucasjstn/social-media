e14de6aa27eebbf71e1268666a5b58159e09c250
lucasjstn <lucas7justino@gmail.com>
2023-01-26 15:00:55 -0300

    creating the route file and starting server

---
+++ backend/models/todo.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const TodoSchema = new Schema({
+    action: {
+        type: String,
+        required: [true, "The todo text field is required"],
+    },
+});
+
+const Todo = mongoose.model("todo", TodoSchema);
+
+export default Todo;
