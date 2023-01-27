25b696dcde551916cf0f4fc35573e4ea73472e86
lucasjstn <lucas7justino@gmail.com>
2023-01-27 20:07:36 -0300

    running the server

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,9 @@
-import express from "express";
-import cors from "cors";
+const colors = require("colors");
+const dotenv = require("dotenv");
 
-const app = express();
+const app = require('./src/app')
 
-// middlewares
-app.use(express.json());
-app.use(cors())
-
-const port = 8000;
-
-// HTTP GET Requested
-app.get('/', (req, res) => {
-    res.status(201).json("HOME GET REQUEST")
-})
-
-app.listen(port, ()=> {
-    console.log(`Server connected to http://localhost:${port}`)
+const PORT = 3000;
+const server = app.listen(PORT, () => {
+    console.log("Server is running");
 })
